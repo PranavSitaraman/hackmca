161ba9dd0e07219f6dbc5227ce63cb970be90243
Pranav Sitaraman <sitaraman.pranav@gmail.com>
2022-09-24 15:31:48 -0400

    sponsors

--- sections/sponsors.jsx
+++ sections/sponsors.jsx
@@ -8,7 +8,7 @@ export default function SponsorsSection({id}) {
                 <p>We are looking for sponsors for this event! If you or your organization would like to sponsor
                     this event and reach hundreds of motivated, tech-minded high school students, please reach out
                     to <a href={"mailto:mcacsclub@gmail.com"}>mcacsclub@gmail.com</a></p>
-                <img href="https://pranavsitaraman.github.io/hackmca/taskade.jpg" alt="taskade"></img>
+                <img href="https://pranavsitaraman.github.io/taskade.jpg" alt="taskade"></img>
             </div>
         </div>
     )
